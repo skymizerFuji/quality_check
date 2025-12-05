02fc952a4b32c5aa27f85bde0ca0288a563eb6d0
fuji <fuji@skymizer.com>
2025-12-05 17:22:33 +0800

    test

---
+++ quality_check/server/test.py
@@ -0,0 +1,23 @@
+"""
+python benchmark.py -m Qwen/Qwen2.5-1.5B-Instruct -i zh -o unquantized
+python benchmark.py -m Qwen/Qwen2.5-1.5B-Instruct -c ./dataset/OpenSafetyMini.csv -i prompt -o unquantized
+python benchmark.py -m Qwen/Qwen2.5-1.5B-Instruct -c ./dataset/OpenSafetyMini.csv -i prompt -o quantized
+python benchmark.py -m Qwen/Qwen2.5-1.5B-Instruct -c ./dataset/skymizer_chat.csv -i zh -o fp16
+"""
+import argparse
+from openai import OpenAI
+import pandas as pd
+from tqdm import tqdm
+
+# Initialize the OpenAI client with your API key
+custom_endpoint_url = "http://localhost:8010"
+client = OpenAI(base_url=custom_endpoint_url)
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant. When answering, unless requested otherwise, give the answer directly without explaining your thought process."},
+        {"role": "user", "content": "hello"}
+    ]
+)
+output = completion.choices[0].message.content
+print(output)
